ca0e1ca89a00682171ac2d935a2890b94f18b609
ngocphat03 <ngonngocphat@gmail.com>
2022-09-21 01:40:49 +0700

    success controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,9 +7,13 @@ using UnityEngine.SceneManagement;
 interface IController
 {
     public void ChangeScene();
-    public void PopupOn();
-    public void PopupOff();
-    public void SocialNetwork();
+    public void Setting();
+	public void CloseSetting();
+	public void Profile();
+	public void CloseProfile();
+	public void Facebook();
+	public void Instagram();
+	public void Tiktok();
     public void Play();
     public void Pause();
     public void GameOver();
@@ -27,37 +31,81 @@ public class Controller : MonoBehaviour, IController
     {
         SceneManager.LoadScene("Start");
     }
-    public void PopupOn()
+//Show and hide button (UI-Screen & UI-Screen Popup) when we click
+	public GameObject profileButton, profilePopup, settingButton, settingPopup;
+    public void Setting()
     {
+        settingPopup.SetActive(true);
 
     }
-    public void PopupOff()
+    public void CloseSetting()
     {
+        settingPopup.SetActive(false);
+    }
+    public void Profile()
+    {
+        profilePopup.SetActive(true);
+    }
+    public void CloseProfile()
+    {
+        profilePopup.SetActive(false);
 
     }
-    public void SocialNetwork()
+//Cho em xin in4 :333
+    public void Facebook()
     {
-
+        Application.OpenURL("https://www.facebook.com/ngonngocphat");
     }
+    public void Instagram()
+    {
+        Application.OpenURL("https://www.instagram.com/mrfatttttt");
+    }
+    public void Tiktok()
+    {
+        Application.OpenURL("https://www.tiktok.com/@nnp1109");
+    }
+//Choi di! Khong nghien dau. Uy tin ma :333
+    private Player player;
+    float startTime = 1;
     public void Play()
     {
-
+        SoundController.instance.PlayThisSound("swoosh");
+        player.enabled = true;
+        Time.timeScale = startTime;
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+        PrintScore();
     }
+//Ngu dong thoi gian
+    float pauseTime = 0;
     public void Pause()
     {
-
+        Time.timeScale = pauseTime;
+        player.enabled = false;
     }
+//Nuoc mat em roi, tro choi ket thuc
     public void GameOver()
-    {
-
+    {    
+        Pause();
     }
+//Chu quan tinh tien!!!
+    public Text scoreText;
+    public Text scoreClone;
+    public int score { get; private set; }
+
     public void IncreaseScore()
     {
-
+        score++;
+        scoreText.text = score.ToString();
+        scoreClone.text = score.ToString();
     }
+//Lay diem tu man hinh
     public void PrintScore()
     {
-
+        scoreText.text = score.ToString();
+        scoreClone.text = score.ToString();
     }
 //Lam cho chim vo canh
     public SpriteRenderer spriteRenderer;
@@ -83,8 +131,17 @@ public class Controller : MonoBehaviour, IController
         }
         spriteRenderer.sprite = sprites[spriteIndex];
     }
+//Cho chim bay len roi ha xuong
+    private int directionMove = 1;
+    private Vector3 movement;
+    public Vector3 direction;
     public void BirdAutomaticallyMove()
     {
-
+        movement.y += directionMove * (Time.deltaTime / 2);
+        transform.position = movement;
+        if(movement.y > 0.3 || movement.y < -1)
+        {
+            directionMove *= -1;
+        }
     }
 }
